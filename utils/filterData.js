c0bd511379474038991b03a6bb4406d2eaf8d81d
Krisha Maharjan <61048054+KrishaMaharjan-00@users.noreply.github.com>
2021-12-14 20:27:12 +0545

    Update filterData.js

--- utils/filterData.js
+++ utils/filterData.js
@@ -9,14 +9,6 @@ export const filterData = [
     },
     {
       items: [
-        { name: 'Buy', value: 'Phone No:: 9856754211, 9647812450' },
-        { name: 'Rent', value: 'Phone No:: 9856754211, 9647812450' },
-      ],
-      placeholder: 'Agent Info',
-      queryName: 'agent_info',
-    },
-    {
-      items: [
         { name: 'Daily', value: 'daily' },
         { name: 'Weekly', value: 'weekly' },
         { name: 'Monthly', value: 'monthly' },
@@ -160,10 +152,6 @@ export const filterData = [
       {
         name: 'purpose',
         value: purpose,
-      },
-      {
-        name: 'agent_info',
-        value: agent_info,
       },
       {
         name: 'rentFrequency',
